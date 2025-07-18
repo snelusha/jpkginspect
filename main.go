f956c2792e9c31ac024b4e6ed3363b497d2d2347
snelusha <hello@snelusha.dev>
2025-07-18 14:16:36 +0530

    feat: implement file finder for java files

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func findFiles(dir string) ([]string, error) {
+	var files []string
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && strings.HasSuffix(d.Name(), ".java") {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
 
 func main() {
-	fmt.Println("hello there!")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("no directory provided!")
+		return
+	}
+
+	dir := args[0]
+
+	files, err := findFiles(dir)
+	if err != nil {
+		fmt.Printf("error finding files: %v\n", err)
+		return
+	}
+
+	if len(files) == 0 {
+		fmt.Println("no .java files found")
+		return
+	}
+
+	fmt.Printf("found %d .java files:\n", len(files))
+	for _, file := range files {
+		fmt.Println(file)
+	}
 }
